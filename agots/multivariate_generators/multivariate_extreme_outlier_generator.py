50e2d771498fb02885c1b1300f5a03013f27a4c1
Zane Blanton <zane.blanton@trivago.com>
2020-03-12 17:58:39 +0100

    Adding randomization for anomaly detection methods (tests not changed)

--- agots/multivariate_generators/multivariate_extreme_outlier_generator.py
+++ agots/multivariate_generators/multivariate_extreme_outlier_generator.py
@@ -4,14 +4,19 @@ from .base import MultivariateOutlierGenerator
 
 
 class MultivariateExtremeOutlierGenerator(MultivariateOutlierGenerator):
-    def __init__(self, timestamps=None, factor=8):
-        self.timestamps = [] if timestamps is None else list(sum(timestamps, ()))
-        self.factor = factor
+    def __init__(self, timestamps=None, factor_range=(8, 10)):
+        if factor_range[0] >= factor_range[1]:
+            raise ValueError('First value must be less than second value')
+        timestamps = timestamps or []
+        self.timestamps = timestamps
+        self.min_factor = factor_range[0]
+        self.factor_width = factor_range[1] - factor_range[0]
 
     def get_value(self, current_timestamp, timeseries):
         if current_timestamp in self.timestamps:
             local_std = timeseries.iloc[max(0, current_timestamp - 10):current_timestamp + 10].std()
-            return np.random.choice([-1, 1]) * self.factor * local_std
+            random_factor = np.random.random() * self.factor_width + self.min_factor
+            return np.random.choice([-1, 1]) * random_factor * local_std
         else:
             return 0
 
--- agots/multivariate_generators/multivariate_proportion_shift_outlier_generator.py
+++ agots/multivariate_generators/multivariate_proportion_shift_outlier_generator.py
@@ -3,8 +3,8 @@ import numpy as np
 from .base import MultivariateOutlierGenerator
 
 
-class MultivariateShiftOutlierGenerator(MultivariateOutlierGenerator):
-    def __init__(self, timestamps=None, factor_range=(8, 10)):
+class MultivariateShiftProportionShiftGenerator(MultivariateOutlierGenerator):
+    def __init__(self, timestamps=None, factor_range=(-.995, -0.98)):
         if factor_range[0] >= factor_range[1]:
             raise ValueError('First value must be less than second value')
         timestamps = timestamps or []
@@ -15,7 +15,6 @@ class MultivariateShiftOutlierGenerator(MultivariateOutlierGenerator):
     def add_outliers(self, timeseries):
         additional_values = np.zeros(timeseries.size)
         for start, end in self.timestamps:
-            local_std = timeseries.iloc[max(0, start - 10):end + 10].std()
             random_factor = np.random.random() * self.factor_width + self.min_factor
-            additional_values[list(range(start, end))] += np.random.choice([-1, 1]) * random_factor * local_std
+            additional_values[list(range(start, end))] *= random_factor
         return additional_values
